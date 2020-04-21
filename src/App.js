9db4ccf8f445dddc96afa33c5cec4cfb224f583e
tangentstorm <michal.wallace@gmail.com>
2020-04-21 00:31:21 -0400

    add firebase, and show the task list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import * as firebase from 'firebase/app'
+// import 'firebase/firestore'
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.stopListening = null;
+    this.state = {
+      startup: true,
+      tasks: []
+    };
+  }
+
+  componentDidMount() {
+    // eslint-disable-next-line no-undef
+    let tasks = firebase.firestore().collection('tasks');
+    this.stopListening = tasks.orderBy('ix').onSnapshot(x => this.onSnapshot(x));
+  }
+
+  onSnapshot(docs) {
+    let tasks = [];
+    docs.forEach(doc => {
+      let task = doc.data();
+      task.task = task.name;
+      task.id = doc.id;
+      tasks.push(task)
+    });
+    this.setState((_old, _props) => { return {tasks}});
+  }
+
+  componentWillUnmount() {
+    if (this.stopListening) this.stopListening();
+  }
+
+  render() {
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+        {this.state.tasks.map(t => (
+            <li key={t.id}>{t.name}</li>
+        ))}
+        </ul>
       </header>
-    </div>
-  );
+    </div>)
+  }
 }
 
 export default App;
